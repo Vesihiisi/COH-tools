10bb4efb8bcbd169ef8553d6d72070356d7e549e
alicia <alicia@fagerving.se>
2016-12-22 12:16:47 +0100

    guess item from wikilink

--- create_distinct_lookup_table.py
+++ create_distinct_lookup_table.py
@@ -1,19 +1,39 @@
 import pymysql
 import argparse
 import wlmhelpers
-
+import pywikibot as pwb
+import mwparserfromhell as mwp
 
 def makeQuery(column, tablename):
     return "SELECT {}, COUNT(*) AS count FROM `{}` GROUP BY {} ORDER BY count DESC".format(column, tablename, column)
 
+def wdItemTemplate(wditemID):
+    return "{{" + wditemID[0] + "|" + wditemID + "}}"
 
-def createWikitable(distinctsTuple):
+def createWikitable(language, distinctsTuple):
     tableTop = "{| class='wikitable sortable'\n! Name\n! Count\n! Item(s)\n! Notes\n"
     tableBottom = "|}\n"
     wikitext = ""
+    site = pwb.Site(language, "wikipedia")
     for item in distinctsTuple:
         if len(item[0]) > 0:
-            wikitext += "|- \n| {}\n| {}\n| \n| \n".format(item[0], item[1])
+            wdItems = []
+            parsedLine = mwp.parse(item[0])
+            wikilinks = parsedLine.filter_wikilinks()
+            for wl in wikilinks:
+                page = pwb.Page(site, wl.title)
+                try:
+                    wdItem = pwb.ItemPage.fromPage(page)
+                    wdItemTemplated = wdItemTemplate(wdItem.getID())
+                    wdItems.append(wdItemTemplated)
+                except pwb.exceptions.NoPage:
+                    pass
+            if len(wdItems) == 0:
+                wdItemsString = ""
+            else:
+                wdItemsString = " ".join(wdItems)
+            print(wdItemsString)
+            wikitext += "|- \n| {}\n| {}\n| {}\n| \n".format(item[0], item[1], wdItemsString)
     return tableTop + wikitext + tableBottom
 
 
@@ -28,10 +48,12 @@ def main(arguments):
         password=arguments.password,
         db=arguments.db,
         charset="utf8")
+    language = wlmhelpers.getLanguage(arguments.table)
     query = makeQuery(arguments.column, arguments.table)
     distincts = wlmhelpers.selectQuery(query, connection)
-    wlmhelpers.saveToFile(
-        createFileName(arguments.column, arguments.table), createWikitable(distincts))
+    filename = createFileName(arguments.column, arguments.table)
+    wikitable = createWikitable(language, distincts)
+    wlmhelpers.saveToFile(filename, wikitable)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
