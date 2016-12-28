e9ab71b7b01bec0dc348eaf74c917051728b4f4a
alicia <alicia@fagerving.se>
2016-12-28 14:10:02 +0100

    load mapping from json

--- importer.py
+++ importer.py
@@ -2,8 +2,12 @@ import argparse
 import sqlalchemy
 from sqlalchemy.ext.declarative import declarative_base
 import wlmhelpers
+import json
+from os import path
 
 Base = declarative_base()
+
+MAPPING_DIR = "mappings/"
 
 
 def run(stmt):
@@ -21,6 +25,16 @@ def get_specific_table_name(countryname, languagename):
     return "monuments_{}_({})".format(countryname, languagename)
 
 
+def load_mapping_file(countryname, languagename):
+    filename = path.relpath(MAPPING_DIR + "{}_({}).json".format(countryname, languagename))
+    try:
+        with open(filename) as f:
+            data = json.load(f)
+        return data
+    except OSError as e:
+        print("File {} does not exist.".format(filename))
+
+
 def main(arguments):
     db = sqlalchemy.create_engine('mysql://{}:{}@{}/{}?charset=utf8'.format(
         arguments.user,
@@ -36,8 +50,7 @@ def main(arguments):
             arguments.country, arguments.language)
         monuments_country = get_monuments_in_country(
             arguments.country, arguments.language, all_monuments)
-        for x in monuments_country:
-            print(x.id, x.name)
+        mapping_file = load_mapping_file(arguments.country, arguments.language)
 
     except sqlalchemy.exc.SQLAlchemyError as exc:
         print("{} does not exist.".format(arguments.table))
