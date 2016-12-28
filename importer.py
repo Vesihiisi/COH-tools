802b95c9a314bd47cabddc4fafe35298a3a31b2a
alicia <alicia@fagerving.se>
2016-12-28 12:10:00 +0100

    work from monuments_all to regional table

--- importer.py
+++ importer.py
@@ -6,6 +6,13 @@ import wlmhelpers
 def run(stmt):
     return stmt.execute()
 
+def create_monuments_table(db):
+    metadata = sqlalchemy.MetaData(db)
+    return sqlalchemy.Table("monuments_all", metadata, autoload=True)
+
+def get_monuments_in_country(countryname, all_monuments):
+    return run(all_monuments.select(all_monuments.c.country == countryname))
+
 
 def main(arguments):
     db = sqlalchemy.create_engine('mysql://{}:{}@{}/{}'.format(
@@ -14,13 +21,11 @@ def main(arguments):
         arguments.host,
         arguments.db), encoding='utf8')
     db.echo = False
-    metadata = sqlalchemy.MetaData(db)
     try:
-        monuments = sqlalchemy.Table(arguments.table, metadata, autoload=True)
-        allMonuments = run(monuments.select())
-        for v in allMonuments:
-            for column, value in v.items():
-                print('{0}: {1}'.format(column, value))
+        all_monuments = create_monuments_table(db)
+        monuments_country = get_monuments_in_country(arguments.country, all_monuments)
+        for r in monuments_country:
+            print(r.name)
     except sqlalchemy.exc.SQLAlchemyError as exc:
         print("{} does not exist.".format(arguments.table))
 
@@ -30,6 +35,6 @@ if __name__ == "__main__":
     parser.add_argument("--user", default="root")
     parser.add_argument("--password", default="")
     parser.add_argument("--db", default="wlm")
-    parser.add_argument("--table", required=True)
+    parser.add_argument("--country")
     args = parser.parse_args()
     main(args)
