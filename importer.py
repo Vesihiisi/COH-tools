727d7281ad55208b7596c1b667cfa9329754bffd
alicia <alicia@fagerving.se>
2016-12-29 11:31:47 +0100

    simplify with raw sql

--- importer.py
+++ importer.py
@@ -1,24 +1,11 @@
 import argparse
-import sqlalchemy
-from sqlalchemy.ext.declarative import declarative_base
+import pymysql
 import json
 from os import path
-
-Base = declarative_base()
+import wlmhelpers
 
 MAPPING_DIR = "mappings/"
 MONUMENTS_ALL = "monuments_all"
-
-
-def run(stmt):
-    return stmt.execute()
-
-
-def get_monuments_in_country(countryname, languagename, all_monuments):
-    return run(all_monuments.select(sqlalchemy.and_(
-        all_monuments.c.country == countryname,
-        all_monuments.c.lang == languagename)
-    )).fetchall()
 
 
 def get_specific_table_name(countryname, languagename):
@@ -39,43 +26,29 @@ def load_mapping_file(countryname, languagename):
         print("File {} does not exist.".format(filename))
 
 
-def create_database(arguments):
-    return sqlalchemy.create_engine('mysql://{}:{}@{}/{}?charset=utf8'.format(
-        arguments.user,
-        arguments.password,
-        arguments.host,
-        arguments.db), encoding='utf8')
-
-
-def table_exists(engine, tablename):
-    return engine.dialect.has_table(engine, tablename)
-
-
-def get_table_autoload(metadata, tablename):
-    return sqlalchemy.Table(
-        tablename, metadata, autoload=True)
+def make_query(country, language, specific_table, join_id):
+    return ('select *  from {} as m_all JOIN `{}` '
+            'as m_spec on m_all.id = m_spec.{} '
+            'WHERE m_all.country="{}" and m_all.lang="{}"'
+            ).format(MONUMENTS_ALL, specific_table, join_id, country, language)
 
 
 def main(arguments):
-    db = create_database(arguments)
+    connection = pymysql.connect(
+        host=arguments.host,
+        user=arguments.user,
+        password=arguments.password,
+        db=arguments.db,
+        charset="utf8")
     country = arguments.country
     language = arguments.language
-    metadata = sqlalchemy.MetaData(db)
-    all_monuments = get_table_autoload(metadata, MONUMENTS_ALL)
-    associated_table_name = get_specific_table_name(
-        country, language)
-    if not table_exists(db, associated_table_name):
-        print("Table {} does not exist.".format(associated_table_name))
-    else:
-        monuments_country = get_monuments_in_country(
-            country, language, all_monuments)
-        print("Fetched {} entries from monuments_all for {}_({}).".format(
-            len(monuments_country), country, language))
-        associated_table = get_table_autoload(metadata, associated_table_name)
-        monuments_specific = run(associated_table.select()).fetchall()
-        print("Fetched {} entries from {}).".format(
-            len(monuments_specific), associated_table_name))
-        mapping_file = load_mapping_file(country, language)
+    mapping_file = load_mapping_file(country, language)
+    query = make_query(country,
+                       language,
+                       get_specific_table_name(country, language),
+                       mapping_file["_id"])
+    results = wlmhelpers.selectQuery(query, connection)
+    print("Fetched {} items from {}".format(len(results), get_specific_table_name(country, language)))
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
