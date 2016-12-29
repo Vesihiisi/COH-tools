4c62847c7153f9c32e107dba7971e33599a2ff11
alicia <alicia@fagerving.se>
2016-12-29 09:05:25 +0100

    fetch all to list

--- importer.py
+++ importer.py
@@ -18,7 +18,7 @@ def get_monuments_in_country(countryname, languagename, all_monuments):
     return run(all_monuments.select(sqlalchemy.and_(
         all_monuments.c.country == countryname,
         all_monuments.c.lang == languagename)
-    ))
+    )).fetchall()
 
 
 def get_specific_table_name(countryname, languagename):
@@ -58,16 +58,20 @@ def get_table_autoload(metadata, tablename):
 
 def main(arguments):
     db = create_database(arguments)
+    country = arguments.country
+    language = arguments.language
     metadata = sqlalchemy.MetaData(db)
     all_monuments = get_table_autoload(metadata, MONUMENTS_ALL)
     associated_table = get_specific_table_name(
-        arguments.country, arguments.language)
+        country, language)
     if not table_exists(db, associated_table):
         print("Table {} does not exist.".format(associated_table))
     else:
         monuments_country = get_monuments_in_country(
-            arguments.country, arguments.language, all_monuments)
-        mapping_file = load_mapping_file(arguments.country, arguments.language)
+            country, language, all_monuments)
+        print("Fetched {} entries for {}_({}).".format(
+            len(monuments_country), country, language))
+        mapping_file = load_mapping_file(country, language)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
