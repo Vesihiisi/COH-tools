5d8cc2245d845c8811a1668b0ac450ff9033ac6c
alicia <alicia@fagerving.se>
2017-01-15 19:04:45 +0100

    util to write logs to file

---
+++ importer/Logger.py
@@ -0,0 +1,30 @@
+import datetime
+from os import path, makedirs
+
+
+class Logger(object):
+
+    def create_filename(self):
+        return datetime.date.today().strftime("%Y-%m-%d" + ".log")
+
+    def save_line_to_file(self, filename, text):
+        with open(filename, 'a') as f:
+            f.write(text + "\n")
+
+    def get_current_timestamp(self):
+        return datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+    def append_timestamp_to_message(self, message):
+        timestamp = self.get_current_timestamp()
+        return timestamp + "\t" + message
+
+    def __init__(self):
+        directory_path = "logs"
+        filename = self.create_filename()
+        self.file_path = path.join(directory_path, filename)
+        if not path.exists(directory_path):
+            makedirs(directory_path)
+        filename = self.create_filename()
+        if not path.exists(self.file_path):
+            open(self.file_path, 'w').close()
+        return
