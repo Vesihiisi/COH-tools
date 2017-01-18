9a9b7f45c95ed7b7ffa2c65a5aa678e4af6c36cb
alicia <alicia@fagerving.se>
2017-01-18 11:44:49 +0100

    keep online tests separate for faster performance

--- importer/tests/test_importer_utils.py
+++ importer/tests/test_importer_utils.py
@@ -236,25 +236,6 @@ def test_get_number_from_string_2():
     assert get_number_from_string("just text!") == None
 
 
-def test_q_from_wikipedia_1():
-    assert q_from_wikipedia("sv", "Norrala socken") == "Q10602691"
-
-
-def test_q_from_wikipedia_2():
-    assert q_from_wikipedia("sv", "Användare:Vesihiisi") == None
-
-
-def test_q_from_wikipedia_2():
-    assert q_from_wikipedia("sv", "This page does not exist") == None
-
-
-def test_q_from_first_wikilink_1():
-    assert q_from_first_wikilink("sv", ("'''Norrala socken'''"
-                                        " ligger i [[Hälsingland]], ingår sedan 1971 i "
-                                        "[[Söderhamns kommun]] och motsvarar från 2016 "
-                                        "[[Norrala distrikt]].")) == "Q206564"
-
-
 def test_string_is_q_item_1():
     assert string_is_q_item("Q1641992") == True
 
@@ -287,22 +268,6 @@ def test_tuple_is_coords_5():
     assert tuple_is_coords((-56.16, -14.86667)) == True
 
 
-def test_file_is_on_commons_1():
-    assert file_is_on_commons("Loojangu värvid 2.jpg") == True
-
-
-def test_file_is_on_commons_2():
-    assert file_is_on_commons("adgffhgadftgfhsfgdg") == False
-
-
-def test_commonscat_exists_1():
-    assert commonscat_exists("Libraries in Germany by city") == True
-
-
-def test_commonscat_exists_2():
-    assert commonscat_exists("adgafgtaffrsdf") == False
-
-
 def test_is_valid_url_1():
     assert is_valid_url(
         "http://pywikibot.readthedocs.io/en/latest/_modules/pywikibot/page/?highlight=WbQuantity") == True
@@ -310,38 +275,6 @@ def test_is_valid_url_1():
 
 def test_get_last_char_1():
     assert get_last_char("foobar") == "r"
-
-
-def test_socken_to_q_1():
-    assert socken_to_q("Holm", "Medelpad") == "Q10525331"
-
-
-def test_socken_to_q_2():
-    assert socken_to_q("Holm", "Uppland") == "Q10525332"
-
-
-def test_socken_to_q_3():
-    assert socken_to_q("Långtora", "Uppland") == "Q10572689"
-
-
-def test_socken_to_q_4():
-    assert socken_to_q("Linde", "Västmanland") == "Q10562482"
-
-
-def test_socken_to_q_5():
-    assert socken_to_q("Löts", "Öland") == "Q10572936"
-
-
-def test_socken_to_q_6():
-    assert socken_to_q("Egby", "Öland") == "Q10480210"
-
-
-def test_socken_to_q_7():
-    assert socken_to_q("Backa", "Bohuslän") == "Q10424141"
-
-
-def test_socken_to_q_8():
-    assert socken_to_q("Solberga", "Bohuslän") == "Q2788890"
 
 
 def test_is_vowel_1():
