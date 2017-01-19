d32f1dc713ba9ed2090d85842c4814a3a3cb708c
alicia <alicia@fagerving.se>
2017-01-19 09:11:39 +0100

    denmark

--- importer/Monument.py
+++ importer/Monument.py
@@ -704,6 +704,18 @@ class DkFortidsDa(Monument):
             except IndexError:
                 return
 
+    def set_inception(self):
+        """
+        TODO
+        these are very broad...
+        basically "prehistoric time", "modern time"
+        is this even useful?
+        """
+        if self.has_non_empty_attribute("datering"):
+            if len(get_wikilinks(self.datering)) == 1:
+                print(self.datering)
+                print(q_from_first_wikilink("da", self.datering))
+
     def __init__(self, db_row_dict, mapping, data_files=None):
         Monument.__init__(self, db_row_dict, mapping, data_files)
         self.update_labels()
@@ -714,6 +726,7 @@ class DkFortidsDa(Monument):
         self.set_coords(("lat", "lon"))
         self.set_adm_location()
         self.set_type()
+        # self.set_inception()
         # self.set_location()
         # self.set_sagsnr()
         # self.set_address()
@@ -793,10 +806,8 @@ class EeEt(Monument):
 class PlPl(Monument):
 
     def update_labels(self):
-        """
-        TODO
-        """
-        return
+        name = remove_markup(self.nazwa)
+        self.add_label("pl", name)
 
     def set_adm_location(self):
         """
@@ -817,11 +828,26 @@ class PlPl(Monument):
         TODo
         Examples of what these can look like: http://tinyurl.com/jz5vuc4
         isolate the date and use as start_time
+        sometimes there are two values:
+        250 z 16.08.1957, 8 z 17.02.1981 (WUOZ - A/463)
+        first step: split on comma OR semicolon?
+        or start by isolating the dates and work backwards
         """
+        # print(self.numer)
         return
 
     def set_address(self):
-        return
+        """
+        NOTE
+        Sometimes this is broken and only a number is included,
+        like "70".
+        But then you can't just test for if it's only number
+        because sometimes it's like "70 B"
+        Check for length? Min 5 characters?
+        """
+        if self.has_non_empty_attribute("adres") and len(self.adres) > 5:
+            street = remove_markup(self.adres)
+            self.add_statement("located_street", street)
 
     def set_location(self):
         settlements_dict = self.data_files["settlements"]
@@ -973,6 +999,7 @@ class CzCs(Monument):
         # self.set_no()
         # self.set_location()
         self.print_wd()
+
 
 class HuHu(Monument):
 
