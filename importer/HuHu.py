f3ea6f14b13d8452f7f19fcee86036fd3b8d7fa5
alicia <alicia@fagerving.se>
2017-01-29 18:52:51 +0100

    separate country

---
+++ importer/HuHu.py
@@ -0,0 +1,24 @@
+from Monument import Monument
+import importer_utils as utils
+
+class HuHu(Monument):
+
+    def update_labels(self):
+        name = utils.remove_markup(self.name)
+        self.add_label("hu", name)
+
+    def set_adm_location(self):
+        return
+
+    def __init__(self, db_row_dict, mapping, data_files, existing):
+        Monument.__init__(self, db_row_dict, mapping, data_files, existing)
+        self.update_labels()
+        # self.exists("sq")
+        self.set_commonscat()
+        self.set_image("image")
+        self.set_coords(("lat", "lon"))
+        self.set_adm_location()
+        # self.set_no()
+        # self.set_location()
+        self.exists_with_prop(mapping)
+        self.print_wd()
--- importer/importer.py
+++ importer/importer.py
@@ -1,6 +1,7 @@
 from SeArbetslSv import SeArbetslSv
 from SeShipSv import SeShipSv
 from PtPt import PtPt
+from HuHu import HuHu
 from Uploader import *
 from Logger import *
 from os import path
@@ -53,7 +54,7 @@ SPECIFIC_TABLES = {"monuments_se-ship_(sv)": {"class": SeShipSv,
                                               {"functions":
                                                "se-ship_(sv)_functions.json"}},
                    # "monuments_cz_(cs)": {"class": CzCs, "data_files": {}},
-                   # "monuments_hu_(hu)": {"class": HuHu, "data_files": {}},
+                   "monuments_hu_(hu)": {"class": HuHu, "data_files": {}},
                    "monuments_pt_(pt)": {"class": PtPt, "data_files": {}},
                    # "monuments_ro_(ro)": {"class": RoRo, "data_files": {}},
                    # "monuments_xk_(sq)": {"class": XkSq, "data_files": {}},
