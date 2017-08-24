c879fae05e89224eeac6aa8dcceee640b1a1c8dc
alicia <alicia@fagerving.se>
2017-08-24 10:15:46 +0200

    tweaks

--- importer/DeHeDe.py
+++ importer/DeHeDe.py
@@ -9,8 +9,33 @@ class DeHeDe(Monument):
         city_q = utils.q_from_wikipedia("de", self.stadt)
         self.add_statement("located_adm", city_q)
 
+    def update_descriptions(self):
+        placename = None
+        germany = {
+            "de": "Deutschland",
+            "en": "Germany",
+            "sv": "Tyskland"
+        }
+        templates = {
+            "en": "Kulturdenkmal in {}",
+            "de": "Kulturdenkmal in {}",
+            "sv": "kulturminnesmärke i {}"
+        }
+        for lg in templates:
+            country = germany[lg]
+            if self.has_non_empty_attribute("stadt"):
+                placename = utils.get_rid_of_brackets(self.stadt)
+                placename = "{}, {}".format(placename, country)
+            else:
+                placename = country
+            desc = templates[lg].format(placename)
+            self.add_description(lg, desc)
+
     def update_labels(self):
-        german = utils.remove_markup(self.bezeichnung)
+        if self.has_non_empty_attribute("bezeichnung"):
+            german = utils.remove_markup(self.bezeichnung)
+        elif self.has_non_empty_attribute("adresse"):
+            german = utils.remove_markup(self.adresse)
         self.add_label("de", german)
 
     def set_heritage_id(self):
@@ -18,7 +43,7 @@ class DeHeDe(Monument):
 
     def set_street_address(self):
         street_with_no = utils.remove_markup(self.adresse)
-        town = self.stadt
+        town = utils.get_rid_of_brackets(self.stadt)
         address = "{}, {}".format(street_with_no, town)
         self.add_statement("located_street", address)
 
@@ -45,6 +70,7 @@ class DeHeDe(Monument):
         self.set_heritage_id()
         self.set_street_address()
         self.update_labels()
+        self.update_descriptions()
         self.set_wd_item(self.find_matching_wikidata(mapping))
 
 
