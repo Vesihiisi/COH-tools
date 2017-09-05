8f8a30b61e79fa1ebf98b04efda8b78c938d4e5a
alicia <alicia@fagerving.se>
2017-09-05 16:51:04 +0200

    some cleanup

--- importer/DeHeDe.py
+++ importer/DeHeDe.py
@@ -53,16 +53,13 @@ class DeHeDe(Monument):
         address = "{}, {}".format(street_with_no, town)
         self.add_statement("located_street", address)
 
-    def set_monuments_all_id(self):
-        self.monuments_all_id = str(self.nummer)
-
     def exists_with_monument_article(self, language):
         return super().exists_with_monument_article("de", "artikel")
 
     def __init__(self, db_row_dict, mapping, data_files, existing, repository):
         Monument.__init__(self, db_row_dict, mapping,
                           data_files, existing, repository)
-        self.set_monuments_all_id()
+        self.set_monuments_all_id("nummer")
         self.set_changed()
         self.wlm_source = self.create_wlm_source(self.monuments_all_id)
         self.set_source()
@@ -72,7 +69,7 @@ class DeHeDe(Monument):
         self.set_country()
         self.set_adm_location()
         self.set_location()
-        self.set_coords(("lat", "lon"))
+        self.set_coords()
         self.set_heritage()
         self.set_heritage_id()
         self.set_street_address()
@@ -85,6 +82,4 @@ if __name__ == "__main__":
     """Point of entrance for importer."""
     args = importer.handle_args()
     dataset = Dataset("de-he", "de", DeHeDe)
-    dataset.data_files = {}
-    dataset.lookup_downloads = {}
     importer.main(args, dataset)
