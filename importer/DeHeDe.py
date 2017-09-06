36ce6a3432b1145460b02d3fcb95d777dce310ce
alicia <alicia@fagerving.se>
2017-09-06 09:11:38 +0200

    rv comm

--- importer/DeHeDe.py
+++ importer/DeHeDe.py
@@ -10,10 +10,16 @@ class DeHeDe(Monument):
             location_q = utils.q_from_wikipedia("de", self.ortsteil)
             if location_q:
                 self.add_statement("location", location_q)
+            else:
+                self.add_to_report("ortsteil", self.ortsteil, "location")
 
     def set_adm_location(self):
+        city_q = None
         city_q = utils.q_from_wikipedia("de", self.stadt)
-        self.add_statement("located_adm", city_q)
+        if city_q:
+            self.add_statement("located_adm", city_q)
+        else:
+            self.add_to_report("stadt", self.stadt, "located_adm")
 
     def update_descriptions(self):
         placename = None
@@ -27,15 +33,15 @@ class DeHeDe(Monument):
             "de": "Kulturdenkmal in {}",
             "sv": "kulturminnesmärke i {}"
         }
-        for lg in templates:
-            country = germany[lg]
+        for language in templates:
+            country = germany[language]
             if self.has_non_empty_attribute("stadt"):
                 placename = utils.get_rid_of_brackets(self.stadt)
                 placename = "{}, {}".format(placename, country)
             else:
                 placename = country
-            desc = templates[lg].format(placename)
-            self.add_description(lg, desc)
+            desc = templates[language].format(placename)
+            self.add_description(language, desc)
 
     def update_labels(self):
         if self.has_non_empty_attribute("bezeichnung"):
@@ -45,9 +51,21 @@ class DeHeDe(Monument):
         self.add_label("de", german)
 
     def set_heritage_id(self):
-        self.add_statement("denkXweb", str(self.nummer))
+        described_qual = {"described_at_url": self.registrant_url}
+        self.add_statement("denkXweb", str(self.nummer), described_qual)
 
     def set_street_address(self):
+        """
+        Add street address property.
+
+        The raw data contains street names and numbers,
+        but no settlements.
+        'Stadt' sometimes contains parentheses in order
+        to serve as a wp link correctly, eg. "Staufenberg (Hessen)",
+        so that part is cut off to get a pure settlement
+        name. This is then attached to the street name and
+        number.
+        """
         street_with_no = utils.remove_markup(self.adresse)
         town = utils.get_rid_of_brackets(self.stadt)
         address = "{}, {}".format(street_with_no, town)
