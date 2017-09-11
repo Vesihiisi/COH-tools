58010be323fa9bc92ac4ee12937ccc3175416911
alicia <alicia@fagerving.se>
2017-09-11 13:29:28 +0200

    heritage stuffs

--- importer/HkEn.py
+++ importer/HkEn.py
@@ -1,9 +1,40 @@
+import dateparser
 from Monument import Monument, Dataset
 import importer_utils as utils
 import importer as importer
 
 
 class HkEn(Monument):
+
+    def set_heritage(self):
+        if self.has_non_empty_attribute("declaration_date"):
+            parsed_d = dateparser.parse(self.declaration_date)
+            # will give None when failure
+            if parsed_d:
+                date_dict = utils.datetime_object_to_dict(parsed_d)
+                qualifier = {"start_time": {"time_value": date_dict}}
+                heritage = self.mapping["heritage"]["item"]
+                self.add_statement("heritage_status", heritage, qualifier)
+            else:
+                super.set_heritage()
+        else:
+            super.set_heritage()
+
+    def set_admin_location(self):
+        hkong = "Q8646"
+        self.add_statement("located_adm", hkong)
+
+    def set_location(self):
+        loc_q = None
+        if utils.count_wikilinks(self.location) == 1:
+            loc_q = utils.q_from_first_wikilink("en", self.location)
+
+        if loc_q:
+            self.add_statement("location", loc_q)
+
+    def update_labels(self):
+        english = utils.remove_markup(self.name)
+        self.add_label("en", english)
 
     def exists_with_monument_article(self, language):
         return super().exists_with_monument_article("en", "monument_article")
@@ -15,8 +46,14 @@ class HkEn(Monument):
         self.set_registrant_url()
         self.set_changed()
         self.set_wlm_source()
+        self.set_heritage()
+        self.set_coords()
         self.set_country()
-        self.set_wd_item(self.find_matching_wikidata(mapping))
+        self.set_admin_location()
+        self.set_location()
+        self.set_image()
+        self.update_labels()
+        # self.set_wd_item(self.find_matching_wikidata(mapping))
 
 
 if __name__ == "__main__":
