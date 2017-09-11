1ac6705f0a93eaee79d183305f8557d43d4a8935
alicia <alicia@fagerving.se>
2017-09-11 12:18:07 +0200

    update

--- importer/MxEs.py
+++ importer/MxEs.py
@@ -8,21 +8,20 @@ class MxEs(Monument):
     def set_heritage_id(self):
         self.add_statement("wlm_id", str(self.id))
 
-    def set_monuments_all_id(self):
-        """Map which column name in specific table to  ID in monuments_all."""
-        self.monuments_all_id = str(self.id)
-
     def exists_with_monument_article(self, language):
         return super().exists_with_monument_article("es", "monument_article")
 
     def __init__(self, db_row_dict, mapping, data_files, existing, repository):
         Monument.__init__(self, db_row_dict, mapping,
                           data_files, existing, repository)
-        self.set_monuments_all_id()
+        self.set_monuments_all_id(str(self.id))
         self.set_changed()
-        self.wlm_source = self.create_wlm_source(self.monuments_all_id)
+        self.set_wlm_source()
         self.set_heritage_id()
         self.set_heritage()
+        self.set_image("imagen")
+        self.set_commonscat("monumento_categoria")
+        self.set_coords()
         self.set_country()
         self.set_is()
         self.set_wd_item(self.find_matching_wikidata(mapping))
@@ -33,5 +32,4 @@ if __name__ == "__main__":
     args = importer.handle_args()
     dataset = Dataset("mx", "ex", MxEs)
     dataset.data_files = {}
-    dataset.lookup_downloads = {}
     importer.main(args, dataset)
