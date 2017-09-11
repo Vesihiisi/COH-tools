27c45659abab2aab807924efbeb8ca911f0dcac9
alicia <alicia@fagerving.se>
2017-09-11 13:05:50 +0200

    start

---
+++ importer/HkEn.py
@@ -0,0 +1,26 @@
+from Monument import Monument, Dataset
+import importer_utils as utils
+import importer as importer
+
+
+class HkEn(Monument):
+
+    def exists_with_monument_article(self, language):
+        return super().exists_with_monument_article("en", "monument_article")
+
+    def __init__(self, db_row_dict, mapping, data_files, existing, repository):
+        Monument.__init__(self, db_row_dict, mapping,
+                          data_files, existing, repository)
+        self.set_monuments_all_id("id")
+        self.set_registrant_url()
+        self.set_changed()
+        self.set_wlm_source()
+        self.set_country()
+        self.set_wd_item(self.find_matching_wikidata(mapping))
+
+
+if __name__ == "__main__":
+    """Point of entrance for importer."""
+    args = importer.handle_args()
+    dataset = Dataset("hk", "en", HkEn)
+    importer.main(args, dataset)
