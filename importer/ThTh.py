8247ef8ac2ee269d243ccf8c46c3f92d4363c251
alicia <alicia@fagerving.se>
2017-09-21 13:47:58 +0200

    update

--- importer/ThTh.py
+++ importer/ThTh.py
@@ -5,6 +5,34 @@ import importer as importer
 
 class ThTh(Monument):
 
+    def set_adm_location(self):
+        adm_q = None
+        prov_dic = self.data_files["provinces"]
+        iso = self.prov_iso
+
+        adm_match = utils.get_item_from_dict_by_key(dict_name=prov_dic,
+                                                    search_term=iso,
+                                                    search_in="iso")
+        if len(adm_match) == 1:
+            adm_q = adm_match[0]
+
+        if adm_q is not None:
+            self.add_statement("located_adm", adm_q)
+        else:
+            self.add_to_report("prov_iso", self.prov_iso, "located_adm")
+
+    def set_heritage_id(self):
+        wlm = self.mapping["table_name"].upper()
+        self.add_statement("wlm_id", "{}-{}".format(wlm, self.register))
+
+    def update_labels(self):
+        th = utils.remove_markup(self.name)
+        self.add_label("th", th)
+
+    def update_descriptions(self):
+        en = "cultural heritage site in Thailand"
+        self.add_description("en", en)
+
     def set_wd_from_name(self):
         if utils.count_wikilinks(self.name) == 1:
             self.set_wd_item(utils.q_from_first_wikilink("th", self.name))
@@ -12,14 +40,19 @@ class ThTh(Monument):
     def __init__(self, db_row_dict, mapping, data_files, existing, repository):
         Monument.__init__(self, db_row_dict, mapping,
                           data_files, existing, repository)
-        self.set_monuments_all_id("id")
+        self.set_monuments_all_id("register")
         self.set_changed()
         self.set_wlm_source()
         self.set_country()
+        self.set_adm_location()
         self.set_is()
         self.set_heritage()
         self.set_heritage_id()
+        self.set_commonscat()
+        self.set_image()
+        self.set_coords()
         self.set_wd_from_name()
+        self.update_labels()
         self.set_wd_item(self.find_matching_wikidata(mapping))
 
 
