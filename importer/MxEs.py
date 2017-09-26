94f5e16b2669cf8103b0edf34d3f1ce9eaf1865e
alicia <alicia@fagerving.se>
2017-09-26 11:09:29 +0200

    update

--- importer/MxEs.py
+++ importer/MxEs.py
@@ -6,6 +6,7 @@ import importer as importer
 class MxEs(Monument):
 
     def set_location(self):
+        """Set the Location, using linked 'localidad'."""
         loc_q = None
         loc_raw = self.localidad
         if utils.count_wikilinks(loc_raw) == 1:
@@ -15,13 +16,32 @@ class MxEs(Monument):
             self.add_statement("location", loc_q)
 
     def set_adm_location(self):
+        """
+        Set the Admin Location.
+
+        Try to use a linked 'municipio',
+        and if that doesn't work, resolve the state
+        via its iso code.
+        """
         adm_q = None
         munic_raw = self.municipio
+        state_dic = self.data_files["states"]
+
         if utils.count_wikilinks(munic_raw) == 1:
             adm_q = utils.q_from_first_wikilink("es", munic_raw)
 
-        if adm_q:
+        if adm_q is None:
+            iso = self.iso
+            state_match = utils.get_item_from_dict_by_key(dict_name=state_dic,
+                                                          search_in="iso",
+                                                          search_term=iso)
+            if len(state_match) == 1:
+                adm_q = state_match[0]
+
+        if adm_q is not None:
             self.add_statement("located_adm", adm_q)
+        else:
+            self.add_to_report("municipio", self.municipio, "located_adm")
 
     def set_heritage_id(self):
         self.add_statement("wlm_id", str(self.id))
@@ -51,5 +71,5 @@ if __name__ == "__main__":
     """Command line entry point for importer."""
     args = importer.handle_args()
     dataset = Dataset("mx", "es", MxEs)
-    dataset.data_files = {}
+    dataset.data_files = {"states": "mexico_states.json"}
     importer.main(args, dataset)
