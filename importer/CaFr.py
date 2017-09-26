d238d094e43adc12f47317e4c9cc356e0709aaf8
alicia <alicia@fagerving.se>
2017-09-26 12:52:10 +0200

    cleanup

--- importer/CaFr.py
+++ importer/CaFr.py
@@ -63,19 +63,15 @@ class CaFr(Monument):
     def set_heritage_id(self):
         self.add_statement("canadian_register", str(self.numero))
 
-    def set_monuments_all_id(self):
-        """Map which column name in specific table to  ID in monuments_all."""
-        self.monuments_all_id = str(self.numero)
-
     def exists_with_monument_article(self, language):
         return super().exists_with_monument_article("fr", "monument_article")
 
     def __init__(self, db_row_dict, mapping, data_files, existing, repository):
         Monument.__init__(self, db_row_dict, mapping,
                           data_files, existing, repository)
-        self.set_monuments_all_id()
+        self.set_monuments_all_id("numero")
         self.set_changed()
-        self.wlm_source = self.create_wlm_source(self.monuments_all_id)
+        self.set_wlm_source()
         self.set_heritage_id()
         self.set_heritage()
         self.set_country()
@@ -84,10 +80,10 @@ class CaFr(Monument):
         self.set_address()
         self.set_is()
         self.set_image()
+        self.set_coords()
         self.set_commonscat()
-        self.set_coords(("lat", "lon"))
         self.set_inception()
-        # self.set_wd_item(self.find_matching_wikidata(mapping))
+        self.set_wd_item(self.find_matching_wikidata(mapping))
 
 
 if __name__ == "__main__":
